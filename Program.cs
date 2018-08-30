d4f4545dd27c277418f064e8a33c03d12a645367
bwahler <bmwahler@gmail.com>
2018-08-29 23:21:59 -0400

    Comments added

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace Lab1
             String input1 = Console.ReadLine();
             int num1;
             int.TryParse(input1, out num1);
+
+            //validation is used so exceptions are through
+
             while (!int.TryParse(input1, out num1))
             {
                 Console.WriteLine("Invalid! Please enter a value");
@@ -23,49 +26,52 @@ namespace Lab1
             String input2 = Console.ReadLine();
             int num2;
             int.TryParse(input2, out num2);
+
+            //validation is used so exceptions are through
+
             while (!int.TryParse(input2, out num2))
             {
                 Console.WriteLine("Invalid! Please enter a value");
                 input2 = Console.ReadLine();
                 int.TryParse(input2, out num2);
             }
-                num2 = Convert.ToInt16(input2);
+            num2 = Convert.ToInt16(input2);
 
-                List<int> listNum1 = new List<int>();
-                while (num1 > 0)
-                {
-                    listNum1.Add(num1 % 10);
-                    num1 = num1 / 10;
-                }
-
-                List<int> listNum2 = new List<int>();
-                while (num2 > 0)
-                {
-                    listNum2.Add(num2 % 10);
-                    num2 = num2 / 10;
-                }
-
-                int[] listArray = new int[listNum1.Count];
-                for (int i = 0; i < listNum1.Count; i++)
-                {
-                    listArray[i] = listNum1[i] + listNum2[i];
-
-                }
-
-                string result = "";
-                for (int i = 0; i < listArray.Length-1; i++)
-                {
-                    if (listArray[i] == listArray[i+1])
-                    {
-                        result = "True";
-                    }
-                    else
-                    {
-                        result = "False";
-                    }
-                }
-                Console.WriteLine(result);
+            List<int> listNum1 = new List<int>();
+            while (num1 > 0)
+            {
+                listNum1.Add(num1 % 10);
+                num1 = num1 / 10;
             }
+
+            List<int> listNum2 = new List<int>();
+            while (num2 > 0)
+            {
+                listNum2.Add(num2 % 10);
+                num2 = num2 / 10;
+            }
+
+            //Array used to compare the two lists
+             
+            int[] listArray = new int[listNum1.Count];
+            for (int i = 0; i < listNum1.Count; i++)
+            {
+                listArray[i] = listNum1[i] + listNum2[i];
+
+            }
+            string result = "";
+            for (int i = 0; i < listArray.Length - 1; i++)
+            {
+                if (listArray[i] == listArray[i + 1])
+                {
+                    result = "True";
+                }
+                else
+                {
+                    result = "False";
+                }
+            }
+            Console.WriteLine(result);
         }
     }
-
+}
